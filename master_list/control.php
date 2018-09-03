9ea4102d412b6a0ec5c8071076da535d1b52558a
Arnel Forbes <forbesarnel09@gmail.com>
2018-09-03 11:49:01 +0900

    modified folders

--- master_list/control.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-if( is_null($_SESSION["id"]) )  {
-	header('location: ../log_in');
-}
-?>
--- master_list/index.php
+++ master_list/index.php
@@ -1,4 +1,4 @@
-<?php require_once '../../forbes/system_control.php'?>
+<?php //require_once '../../forbes/system_control.php'?>
 <!DOCTYPE html>
 <html lang="en" oncontextmenu="return false">
 	<head>
@@ -7,17 +7,18 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 		<link rel="icon" href="">
 		<title>Master List</title>
-		<link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet">
-		<link href="sheet/general.css" rel="stylesheet">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<link href="css/main.css" rel="stylesheet">
 	</head>
 	<body>
 		<div class="container-fluid">
-			<?php require_once 'interface/user_form.php'; ?>
+			<?php //require_once 'interface/user_form.php'; ?>
 			<div id="content"></div>
 		</div>
-		<?php require_once 'interface/modal.php'; ?>
-		<script src="../bootstrap/js/jquery-3.2.1.js"></script>
-		<script src="../bootstrap/js/bootstrap.min.js"></script>
-		<script src="script/master.js"></script>
+		<?php require_once 'ui/modal.php'; ?>
+		
+		<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+		<script src="js/main.js"></script>
 	</body>
 </html>
