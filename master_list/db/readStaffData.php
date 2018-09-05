af70f2e42548a8e176987905b69a6f76bed4569a
Arnel Forbes <forbesarnel09@gmail.com>
2018-09-05 13:53:54 +0900

    done

--- master_list/db/readStaffData.php
+++ master_list/db/readStaffData.php
@@ -28,13 +28,57 @@ $pdo = null;
 ?>
 <div class="row">
 	<div class="col-sm-4">
-		<img width="150px" src="../image/masterList/<?php echo $staff["img"] ?>" alt="'s picture"/>
+		<img width="100%" src="../image/masterList/<?php echo $staff["img"] ?>" alt="'s picture"/>
 	</div>
 	<div class="col-sm-8">
 		<table id="staffDetailTable" class="table table-condensed table-striped">
 			<tr>
-				<td>I.d. No. :</td>
-				<td></td>
+				<td>I.D. No.:</td>
+				<td><?php echo $staff["idNum"] ?></td>
+			</tr>
+			<tr>
+				<td>First Name:</td>
+				<td><?php echo $staff["firstName"] ?></td>
+			</tr>
+			<tr>
+				<td>Middle Name:</td>
+				<td><?php echo $staff["middleName"] ?></td>
+			</tr>
+			<tr>
+				<td>Last Name:</td>
+				<td><?php echo $staff["lastName"] ?></td>
+			</tr>
+			<tr>
+				<td>Nick Name:</td>
+				<td><?php echo $staff["nickName"] ?></td>
+			</tr>
+			<tr>
+				<td>Birth Date:</td>
+				<td><?php echo $staff["birthDate"] ?></td>
+			</tr>
+			<tr>
+				<td>Hired Date:</td>
+				<td><?php echo $staff["hiredDate"] ?></td>
+			</tr>
+			<tr>
+				<td>E-mail:</td>
+				<td><?php echo $staff["email"] ?></td>
+			</tr>
+			<tr>
+				<td>Phone Number:</td>
+				<td><?php echo $staff["phoneNumber"] ?></td>
+			</tr>
+			<tr>
+				<td>Employment Status:</td>
+				<td><?php echo $staff["statDesc"] ?></td>
+			</tr>
+			<tr>
+				<td>Job Description:</td>
+				<td><?php echo $staff["jobName"] ?></td>
+			</tr>
+			<tr>
+				<td>Team:</td>
+				<td><?php echo $staff["teamName"] ?></td>
 			</tr>
 		</table>
 	</div>
