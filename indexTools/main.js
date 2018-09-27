6c7abeb1fc59d0aa2cc098ae03239225d10c2568
Arnel Forbes <forbesarnel09@gmail.com>
2018-09-27 16:54:38 +0900

    sticky nav

--- indexTools/main.js
+++ indexTools/main.js
@@ -1,13 +1,27 @@
-const test = document.querySelector('#test');
-const header = document.querySelector('.header')
+// const test = document.querySelector('#test');
+const header = document.querySelector('.header');
+const nav = document.querySelector('.nav')
+const navY = nav.offsetTop;
+const main = document.querySelector('main');
+const navKey = document.querySelectorAll('[data-target]');
 
 // change image during scrolls
 function setImage(){
 	const scrolled = window.scrollY;
 	const height = header.offsetHeight;
 	let image;
-	scrolled > height ? image = 'url(\'../image/cad1.jpg\')' : image = 'url(\'../image/cad3.jpg\')'
-	document.documentElement.style.setProperty('--image', image)
+
+	scrolled >= navY ? nav.classList.add('fixed') : nav.classList.remove('fixed');
+	scrolled > height ? image = 'url(\'../image/cad1.jpg\')' : image = 'url(\'../image/cad3.jpg\')';
+	document.documentElement.style.setProperty('--image', image);
+}
+
+function navigate(){
+	const target = document.querySelector(`#${this.dataset.target}`);
+	window.scrollTo(0, target.offsetTop - nav.offsetHeight);
 }
 
 document.addEventListener('scroll', setImage);
+navKey.forEach(key => {
+	key.addEventListener('click', navigate);
+})
